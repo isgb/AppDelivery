142d7baf03394784cc3aaf3ce89bb4ddeec8d9f4
oscar <oscar.vasquez@zentro.com.mx>
2024-03-13 16:05:40 -0600

    debug

--- App.tsx
+++ App.tsx
@@ -8,6 +8,9 @@ export default function App() {
         source={ require('./assets/chef.jpg')}
         style={styles.imageBackground}
       />
+      <View style={styles.form}>
+
+      </View>
     </View>
   );
 }
@@ -21,5 +24,12 @@ const styles = StyleSheet.create({
     width:'100%',
     height: '100%',
     opacity: 0.7
+  },
+  form:{
+    width:'100%',
+    height: '40%',
+    backgroundColor: 'white',
+    position:'absolute',
+    bottom: 0
   }
 });
