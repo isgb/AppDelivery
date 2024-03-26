2268b5e2988bc6bc37699d5a6273adf579fb18f0
oscar <oscar.vasquez@zentro.com.mx>
2024-03-26 16:08:29 -0600

    debug

--- App.tsx
+++ App.tsx
@@ -8,7 +8,9 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{
+        headerShown: false
+      }}>
         <Stack.Screen
           name="Home"
           component={HomeScreen}
