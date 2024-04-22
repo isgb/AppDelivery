c69f54be32da9f20e05aff3ebbceec3bb0858d6c
oscar <oscar.vasquez@zentro.com.mx>
2024-04-22 17:20:12 -0600

    debug

--- src/Domain/entities/User.tsx
+++ src/Domain/entities/User.tsx
@@ -1,5 +1,5 @@
 export interface User {
-    id:              string;
+    id?:             string;
     name:            string;
     lastname:        string;
     phone:           string;
--- src/Presentation/views/register/ViewModel.tsx
+++ src/Presentation/views/register/ViewModel.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { ApiDelivery } from '../../../Data/sources/remote/api/ApiDelivery';
+import { RegisterAuthUseCase } from '../../../Domain/useCases/auth/RegisterAuth';
 
 const RegisterViewModel = () => {
 
@@ -17,16 +18,20 @@ const RegisterViewModel = () => {
     }
 
     const register = async () => {
-      try {
+      const { result, error } = await RegisterAuthUseCase(values);
+      console.log("RESULT: " + JSON.stringify(result));
+      console.log("ERROR: " + error);
+      
+      // try {
 
-        const response = await ApiDelivery.post('/users/create',values);
-        console.log("RESPONSE " + JSON.stringify(response));
+      //   const response = await ApiDelivery.post('/users/create',values);
+      //   console.log("RESPONSE " + JSON.stringify(response));
 
-      } catch (error) {
-        console.log("ERROR: " + error);
+      // } catch (error) {
+      //   console.log("ERROR: " + error);
         
-      }
-      console.log(JSON.stringify(values))
+      // }
+      // console.log(JSON.stringify(values))
     }
 
   return {
