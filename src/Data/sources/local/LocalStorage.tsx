4861d6ca704f00f5be22c5a3164b621d7c1fe050
oscar <oscar.vasquez@zentro.com.mx>
2024-04-26 15:40:43 -0600

    debug

--- src/Data/sources/local/LocalStorage.tsx
+++ src/Data/sources/local/LocalStorage.tsx
@@ -1,13 +1,12 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const LocalStorage = () => {
+export const LocalStorage = async () => {
 
-    const save = async (key: string, value:string){
+    const save = async (key:string, value:string){
         try {
             await AsyncStorage.setItem(key, value);
         } catch (error) {
             console.log("ERROR EN EL LOCAL STORAGE: " + error);
-            
         }
     }
 
