9b086c29d0b6f42b8b8cd8820ca367434e25be78
oscar <oscar.vasquez@zentro.com.mx>
2024-05-03 16:02:08 -0600

    debug

--- src/Data/sources/remote/api/ApiDelivery.jsx
+++ src/Data/sources/remote/api/ApiDelivery.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const ApiDelivery = axios.create({
-    baseURL : 'http://192.168.1.67:3000/api',
+    baseURL : 'http://192.168.1.68:3000/api',
     headers: {
         'Content-type' : 'application/json'
     }
