55f1b140ff303c6b78ab7261c856dd5c63cd8f83
oscar <oscar.vasquez@zentro.com.mx>
2024-05-22 16:16:23 -0600

    debug

--- App.tsx
+++ App.tsx
@@ -4,11 +4,13 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { HomeScreen } from './src/Presentation/views/home/Home';
 import { RegisterScreen } from './src/Presentation/views/register/Register';
 import { ProfileInfoScreen } from './src/Presentation/views/profile/info/ProfileInfo';
+import { RolesScreen } from './src/Presentation/views/roles/Roles';
 
 export type RootStackParamList = {
   HomeScreen : undefined,
   RegisterScreen: undefined,
-  ProfileInfoScreen: undefined
+  ProfileInfoScreen: undefined,
+  RolesScreen: undefined
 }
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -34,6 +36,14 @@ const App = () => {
          <Stack.Screen
           name="ProfileInfoScreen"
           component={ProfileInfoScreen}
+        />
+        <Stack.Screen 
+            name="RolesScreen" 
+            component={RolesScreen} 
+            options={{
+              headerShown: true,
+              title: 'Selecciona un Rol'
+            }}    
         />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ src/Presentation/views/roles/Roles.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import useViewModel from './ViewModel';
+
+export const RolesScreen = () => {
+  return (
+    <View>
+        <Text>RolesScreen</Text>
+    </View>
+  )
+}
