56973a0fdfe907f8eed65d85127a7a0fd92a29cc
oscar <oscar.vasquez@zentro.com.mx>
2024-05-23 16:00:54 -0600

    debug

--- src/Presentation/views/roles/Roles.tsx
+++ src/Presentation/views/roles/Roles.tsx
@@ -2,20 +2,46 @@ import React from 'react'
 import { Dimensions, FlatList, Text, View } from 'react-native'
 import useViewModel from './ViewModel';
 import { RolesItem } from './Item';
+import Carousel from 'react-native-reanimated-carousel';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export const RolesScreen = () => {
 
-    const {user} = useViewModel();
-    const width = Dimensions.get('window').width;
-    const height = Dimensions.get('window').height;
+  const { user } = useViewModel();
+  const width = Dimensions.get('window').width;
+  const height = Dimensions.get('window').height;
 
   return (
-    <View>
-        <FlatList
+    <GestureHandlerRootView>
+      <View>
+        {/* <FlatList
             data={ user?.roles}
             renderItem={({item}) => <RolesItem rol={item} height={420} width={width - 100}/>}
             keyExtractor={ (item) => item.id }
+        /> */}
+        <Carousel
+          loop={true}
+          width={width}
+          height={width / 2}
+          autoPlay={true}
+          data={[...new Array(6).keys()]}
+          scrollAnimationDuration={1000}
+          onSnapToItem={(index) => console.log('current index:', index)}
+          renderItem={({ index }) => (
+            <View
+              style={{
+                flex: 1,
+                borderWidth: 1,
+                justifyContent: 'center',
+              }}
+            >
+              <Text style={{ textAlign: 'center', fontSize: 30 }}>
+                {index}
+              </Text>
+            </View>
+          )}
         />
-    </View>
+      </View>
+    </GestureHandlerRootView>
   )
 }
