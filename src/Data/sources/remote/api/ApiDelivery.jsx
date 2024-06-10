149906e48c1061c349c76f7f77736fb221a60acf
oscar <oscar.vasquez@zentro.com.mx>
2024-06-10 16:08:52 -0600

    debug

--- src/Data/sources/remote/api/ApiDelivery.jsx
+++ src/Data/sources/remote/api/ApiDelivery.jsx
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const ApiDelivery = axios.create({
-    baseURL : 'http://192.168.1.64:3000/api',
+    baseURL : 'http://192.168.1.69:3000/api',
     headers: {
         'Content-type' : 'application/json'
     }
 })
 
 const ApiDeliveryForImage = axios.create({
-    baseURL : 'http://192.168.1.64:3000/api',
+    baseURL : 'http://192.168.1.69:3000/api',
     headers: {
         'Content-type' : 'multipart/form-data',
         'accept' : 'application/json'
--- src/Presentation/navigator/AdminTabsNavigator.tsx
+++ src/Presentation/navigator/AdminTabsNavigator.tsx
@@ -1,14 +1,22 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { AdminCategoryListScreen } from '../views/admin/category/list/CategoryList';
 import { AdminOrderListScreen } from '../views/admin/order/list/OrderList';
-import { ProfileInfoScreen } from '../views/profile/info/ProfileInfo';
+import  {ProfileInfoScreen}  from '../views/profile/info/ProfileInfo';
 
 const Tab = createBottomTabNavigator();
 
 export const AdminTabsNavigator = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="AdminCategoryListScreen" component={AdminCategoryListScreen} />
+      <Tab.Screen 
+        name="AdminCategoryListScreen" 
+        component={AdminCategoryListScreen} 
+        options={{
+          title: 'Categorias',
+          tabBarLabel: 'Categorias',
+          tabNa
+        }}
+      />
       <Tab.Screen name="AdminOrderListScreen" component={AdminOrderListScreen} />
       <Tab.Screen name="ProfileInfoScreen" component={ProfileInfoScreen} />
     </Tab.Navigator>
