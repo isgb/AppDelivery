2e4f082a81acc96a1ef71ec809f3f3c43992c1f6
oscar <oscar.vasquez@zentro.com.mx>
2024-06-12 15:09:15 -0600

    debug

--- src/Presentation/views/profile/info/ProfileInfo.tsx
+++ src/Presentation/views/profile/info/ProfileInfo.tsx
@@ -3,15 +3,16 @@ import { Button, Text, View } from 'react-native'
 import useViewModel from './ViewModel'; 
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParamList } from '../../../../../App';
+import styles from './Styles';
 
-interface Props extends StackScreenProps<RootStackParamList, 'ClientTabsNavigator'>{};
+interface Props extends StackScreenProps<RootStackParamList>{};
 
 export const ProfileInfoScreen = ({navigation, route} : Props) => {
 
     const { removeSession } = useViewModel();
 
     return (
-        <View style={{flex: 1, justifyContent:'center',alignItems: 'center'}}>
+        <View style={styles.container}>
             <Button
                 onPress={() =>{
                     removeSession();
---
+++ src/Presentation/views/profile/info/Styles.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+const ProfileInfoStyles = StyleSheet.create({
+    container:{
+        flex: 1
+    }
+});
+
+export default ProfileInfoStyles;
