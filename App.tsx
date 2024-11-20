6507de8b527dd19b9156b210bd24a066926d1667
isaias <isaias.gaona@zentro.com.mx>
2024-11-20 16:25:27 -0600

    avances

--- App.tsx
+++ App.tsx
@@ -1,107 +1,17 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { HomeScreen } from "./src/Presentation/views/home/Home";
-import { RegisterScreen } from "./src/Presentation/views/register/Register";
-// import { ProfileInfoScreen } from './src/Presentation/views/profile/info/ProfileInfo';
-import { RolesScreen } from "./src/Presentation/views/roles/Roles";
-import { AdminTabsNavigator } from "./src/Presentation/navigator/AdminTabsNavigator";
-import { ClientTabsNavigator } from "./src/Presentation/navigator/ClientTabsNavigator";
-import { ProfileUpdateScreen } from "./src/Presentation/views/profile/update/ProfileUpdate";
-import { User } from "./src/Domain/entities/User";
-import { UserProvider } from "./src/Presentation/context/UserContext";
-import { AdminCategoryCreateScreen } from "./src/Presentation/views/admin/category/create/CategoryCreate";
-import { AdminCategoryUpdateScreen } from "./src/Presentation/views/admin/category/update/CategoryUpdate";
-import { Category } from "./src/Domain/entities/Category";
-
-export type RootStackParamList = {
-  HomeScreen: undefined;
-  RegisterScreen: undefined;
-  RolesScreen: undefined;
-  AdminTabsNavigator: undefined;
-  ClientTabsNavigator: undefined;
-  ProfileUpdateScreen: { user: User };
-  AdminCategoryCreateScreen: undefined;
-  AdminCategoryUpdateScreen: {category: Category};
-};
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
+import MainStackNavigator from "./src/Presentation/navigator/MainStackNavigator";
 
 const App = () => {
   return (
     <NavigationContainer>
-      <UserState>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="HomeScreen" component={HomeScreen} />
-          <Stack.Screen
-            name="RegisterScreen"
-            component={RegisterScreen}
-            options={{
-              headerShown: true,
-              title: "Nuevo Usuario",
-            }}
-          />
-          {/* <Stack.Screen
-          name="ProfileInfoScreen"
-          component={ProfileInfoScreen}
-        /> */}
-          <Stack.Screen
-            name="RolesScreen"
-            component={RolesScreen}
-            options={{
-              headerShown: true,
-              title: "Selecciona un Rol",
-            }}
-          />
-
-          <Stack.Screen
-            name="AdminTabsNavigator"
-            component={AdminTabsNavigator}
-          />
-
-          <Stack.Screen
-            name="ClientTabsNavigator"
-            component={ClientTabsNavigator}
-          />
-
-          <Stack.Screen
-            name="ProfileUpdateScreen"
-            component={ProfileUpdateScreen}
-            options={{
-              headerShown: true,
-              title: "Actualizar usuario",
-            }}
-          />
-
-          <Stack.Screen
-            name="AdminCategoryCreateScreen"
-            component={AdminCategoryCreateScreen}
-            options={{
-              headerShown: true,
-              title: "Nueva categoria",
-            }}
-          />
-
-          <Stack.Screen
-            name="AdminCategoryUpdateScreen"
-            component={AdminCategoryUpdateScreen}
-            options={{
-              headerShown: true,
-              title: "Editar categoria",
-            }}
-          />
-        </Stack.Navigator>
-      </UserState>
+      <MainStackNavigator/>
     </NavigationContainer>
   );
 };
 
-const UserState = ({ children }: any) => {
-  return <UserProvider>{children}</UserProvider>;
-};
+// const ContextState = ({ children }: any) => {
+//   return <CategoryProvider>{children}</CategoryProvider>;
+// };
 
 export default App;
--- src/Presentation/context/CategoryContext.tsx
+++ src/Presentation/context/CategoryContext.tsx
@@ -1,52 +1,75 @@
-import { createContext, useState } from "react";
-import { ResponseAPIDelivery } from "../../Data/sources/remote/models/ResponseApiDelivery";
-import { Category } from '../../Domain/entities/Category';
+import { createContext, useState, useEffect } from 'react';
+import { Category } from "../../Domain/entities/Category";
 import * as ImagePicker from 'expo-image-picker';
-import { GetAllCategorUseCase } from "../../Domain/useCases/category/GetAllCategory";
-import { CreateCategoryUseCase } from "../../Domain/useCases/category/CreateCategory";
-import { UpdateCategoryUseCase } from "../../Domain/useCases/category/UpdateCategory";
+import { ResponseAPIDelivery } from "../../Data/sources/remote/models/ResponseApiDelivery";
+import { GetAllCategorUseCase } from '../../Domain/useCases/category/GetAllCategory';
+import { CreateCategoryUseCase } from '../../Domain/useCases/category/CreateCategory';
+import { UpdateCategoryUseCase } from '../../Domain/useCases/category/UpdateCategory';
 import { UpdateWithImageCategoryUseCase } from '../../Domain/useCases/category/UpdateWithImageCategory';
-import { DeleteCategoryUseCase } from "../../Domain/useCases/category/DeleteCategory";
+import { DeleteCategoryUseCase } from '../../Domain/useCases/category/DeleteCategory';
+
 
 export interface CategoryContextProps {
     categories: Category[],
-    getAll(): Promise<Category[]>;
-    create(category: Category, file: ImagePicker.ImageInfo): Promise<ResponseAPIDelivery>;
-    update(category: Category): Promise<ResponseAPIDelivery>;
-    updateWithImage(category: Category, file: ImagePicker.ImageInfo): Promise<ResponseAPIDelivery>;
-    remove(id: String): Promise<ResponseAPIDelivery>
+    getCategories(): Promise<void>,
+    create(category: Category, file: ImagePicker.ImageInfo): Promise<ResponseAPIDelivery>,
+    update(category: Category): Promise<ResponseAPIDelivery>,
+    updateWithImage(category: Category, file: ImagePicker.ImageInfo): Promise<ResponseAPIDelivery>,
+    remove(id: string): Promise<ResponseAPIDelivery>
 }
 
-export const CategoryContext = createContext({} as CategoryContextProps)
+export const CategoryContext = createContext( {} as CategoryContextProps)
 
 export const CategoryProvider = ( {children}: any) => {
-    const [categories, setCategories] = useState<Category[]>([])
 
-    const getCategories = async () => {
-        const result = await GetAllCategorUseCase();
+    const [categories, setCategories] = useState<Category[]>([]);
+
+    useEffect(() => {
+        if (categories.length === 0) {
+            getCategories();
+        }
+      }, []);
+
+    const getCategories = async(): Promise<void> => {
+        const result = await GetAllCategorUseCase();        
         setCategories(result);
     }
 
-    const createCategory = async(category: Category, file: ImagePicker.ImageInfo): Promise<ResponseAPIDelivery> =>{
-        const response = await CreateCategoryUseCase(category,file!);
-        setCategories(categories)
+    const create = async (category: Category, file: ImagePicker.ImageInfo): Promise<ResponseAPIDelivery> => {
+        const response = await CreateCategoryUseCase(category, file!);
+        getCategories();
         return response;
     }
 
     const update = async (category: Category): Promise<ResponseAPIDelivery> => {
         const response = await UpdateCategoryUseCase(category);
-        setCategories(categories);
+        getCategories();
+        return response;
+    }
+    
+    const updateWithImage = async (category: Category, file: ImagePicker.ImageInfo): Promise<ResponseAPIDelivery> => {
+        const response = await UpdateWithImageCategoryUseCase(category, file);
+        getCategories();
         return response;
     }
 
-    const updateWithImage = async (category: Category, file: ImagePicker.ImageInfo): Promise<ResponseAPIDelivery> => {
-        const response = await UpdateWithImageCategoryUseCase(category, file);
-        setCategories(categories);
-        return response;
-    }
     const remove = async (id: string): Promise<ResponseAPIDelivery> => {
         const response = await DeleteCategoryUseCase(id);
-        setCategories(categories);
+        getCategories();
         return response;
     }
+
+    return (
+        <CategoryContext.Provider value={{
+            categories,
+            getCategories,
+            create,
+            update,
+            updateWithImage,
+            remove
+        }}>
+            { children }
+        </CategoryContext.Provider>
+    )
+
 }
